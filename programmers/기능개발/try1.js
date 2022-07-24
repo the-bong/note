c29f7cc558aca421c196e6d1c0fd2279f0783149
bong <cptbbong@gmail.com>
2022-07-24 14:31:12 +0900

    add url.

--- programmers/기능개발/try1.js
+++ programmers/기능개발/try1.js
@@ -1,3 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42586
 function solution(progresses, speeds) {
   const queue = progresses.map((value, i) => Math.ceil((100 - value) / speeds[i]));
 
--- programmers/위장/try1.js
+++ programmers/위장/try1.js
@@ -1,3 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42578
 function solution(clothes) {
   const table = clothes.reduce((accumulator, [_, key]) => {
     accumulator[key] = accumulator[key] ? accumulator[key] + 1 : 1;
