d290ad50afd296483090409f403c899dfe5a96e3
bong <cptbbong@gmail.com>
2022-08-14 18:10:18 +0900

    add Integer to Roman.

---
+++ leetcode/12. Integer to Roman/README.md
@@ -0,0 +1,4 @@
+[12. Integer to Roman](https://leetcode.com/problems/integer-to-roman/)
+
+- 13. Roman to Integer의 변형
+  - 별다를건 없고, .repeat(num / key); 만 추가.
---
+++ leetcode/12. Integer to Roman/try1.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ var intToRoman = function(num) {
+  const map = new Map();
+  map.set(1000, 'M');
+  map.set(900, 'CM');
+  map.set(500, 'D');
+  map.set(400, 'CD');
+  map.set(100, 'C');
+  map.set(90, 'XC');
+  map.set(50, 'L');
+  map.set(40, 'XL');
+  map.set(10, 'X');
+  map.set(9, 'IX');
+  map.set(5, 'V');
+  map.set(4, 'IV');
+  map.set(1, 'I');
+
+  let result = '';
+
+  for (const [key, value] of map) {
+    if (num >= key) {
+      result += value.repeat(num / key); 
+      num = num % key;
+    }
+  }
+
+  return result;
+};
+
+export {intToRoman};
