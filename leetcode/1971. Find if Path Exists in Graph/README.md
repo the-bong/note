9427acdaf2f6a83ebedecb93a50290c60bbb6f27
bong <cptbbong@gmail.com>
2022-08-21 19:17:46 +0900

    add Find if path exists in graph.

---
+++ leetcode/1971. Find if Path Exists in Graph/README.md
@@ -0,0 +1,5 @@
+[1971. Find if Path Exists in Graph](https://leetcode.com/problems/find-if-path-exists-in-graph/)
+
+- DFS연습
+  - try1성공했으나 submit하면, runtime error가 발생하는 경우가 있음
+  - 재귀문제로 판단되어 try2에서 마지막까지 탐색하면 return추가.
---
+++ leetcode/1971. Find if Path Exists in Graph/try2.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+ var validPath = function(n, edges, source, destination) {
+  const graph = new Map();
+  const visited = new Set();
+
+  for(const [left, right] of edges) {
+    if (!graph.get(left)) graph.set(left, []);
+    if (!graph.get(right)) graph.set(right, []);
+    graph.get(left).push(right);
+    graph.get(right).push(left);
+  }
+
+  function helper(value) {
+    visited.add(value);
+    if (value === destination) return;
+    const edges = graph.get(value);
+
+    if (edges && edges.length > 0) {
+      for (const e of edges) {
+          if (!visited.has(e)) helper(e);
+      }
+    }
+  }
+  helper(source);
+  
+  return visited.has(destination);
+};
+
+export {validPath};
